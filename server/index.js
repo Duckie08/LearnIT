da3415f4333088a78ace58180e6d7f0a40cf1f43
Duckie08 <52843939+Duckie08@users.noreply.github.com>
2021-10-02 00:46:41 +0700

    feat: create an user

---
+++ server/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose')
+
+const authRouter = require('./routes/auth')
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.bwwqb.mongodb.net/mern?retryWrites=true&w=majority`)
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.log(error.message)
+    process.exit[1]
+  }
+}
+
+connectDB()
+
+const app = express()
+app.use(express.json())
+
+app.use('/api/auth', authRouter )
+
+
+const PORT = 5000
+
+app.listen(PORT, () => console.log(`Server started pm [prt ${PORT}]`))
